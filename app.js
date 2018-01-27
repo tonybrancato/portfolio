f9f8a37e8afc88077f460d049751bd439f99696e
Tony Brancato <tonybrancato1@gmail.com>
2018-01-27 14:57:10 -0500

    hide nav, jquery refactor

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
-$(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store hash
-        var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-     
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
-    });
+
+
+function smoothScroll() {
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+    
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
   });
+}
+
+function hideNav() {
+  // hide .navbar first
+  $(".navbar").hide();
+
+  // fade in .navbar
+  $(function () {
+      $(window).scroll(function () {
+
+                // set distance user needs to scroll before we start fadeIn
+          if ($(this).scrollTop() > 500) {
+              $('.navbar').fadeIn();
+          } else {
+              $('.navbar').fadeOut();
+          }
+      });
+  });
+}
+
+$(function (){
+  smoothScroll();
+  hideNav();
+})
